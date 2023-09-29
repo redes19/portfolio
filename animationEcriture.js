5817147b4393d8202f69de4089b645e39b6ebb9e
Loris Laurenti <lloris19@outlook.fr>
2023-09-29 11:07:43 +0200

    Update animationEcriture.js

--- animationEcriture.js
+++ animationEcriture.js
@@ -2,7 +2,7 @@ var speed = 200;
 var h1 = document.querySelector("h1");
 
 function typeEffect(e, speed) {
-  var text = e.innerHTML;
+  var text = e.textContent;
   e.innerHTML = "";
 
   var i = 0;
