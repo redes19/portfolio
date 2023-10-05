b56c4ffd7bf24c47468f60cb45fe9be246e70da6
Loris Laurenti <lloris19@outlook.fr>
2023-10-06 00:44:22 +0200

    js carrousel

--- projet/tower-defense/index.js
+++ projet/tower-defense/index.js
@@ -4,4 +4,10 @@ document.body.onload=function(){
     let container = document.getElementById("container");
     let g = document.getElementById("g");
     let d = document.getElementById("d");
+    container.style.width=(800*nb)+"px";
+    for(i=1; i<=nbr; i++){
+        let div = document.createElement("div");
+        div.className="photo";
+        div.backgroundImage="url('')"
+    }
 }
