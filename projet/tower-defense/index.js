d914376eefa80d12bcb46afcba9fe6ed3dd5bdb8
Loris Laurenti <lloris19@outlook.fr>
2023-10-05 19:49:36 +0200

    début carrousel

---
+++ projet/tower-defense/index.js
@@ -0,0 +1,7 @@
+document.body.onload=function(){
+    let nrb = 5;
+    let p = 0;
+    let container = document.getElementById("container");
+    let g = document.getElementById("g");
+    let d = document.getElementById("d");
+}
