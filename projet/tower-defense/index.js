8efcf92135b530281a7fe71801861c3098d2ee44
Loris Laurenti <lloris19@outlook.fr>
2023-10-06 11:50:07 +0200

    finalisation carrousel

--- projet/tower-defense/index.js
+++ projet/tower-defense/index.js
@@ -1,13 +1,49 @@
+let nbr = 3;
+let p = 0;
+let container = document.getElementById("container");
+let g = document.getElementById("g");
+let d = document.getElementById("d");
+
 document.body.onload=function(){
-    let nrb = 5;
-    let p = 0;
-    let container = document.getElementById("container");
-    let g = document.getElementById("g");
-    let d = document.getElementById("d");
-    container.style.width=(800*nb)+"px";
+    container.style.width=(800*nbr)+"px";
+
     for(i=1; i<=nbr; i++){
         let div = document.createElement("div");
         div.className="photo";
-        div.backgroundImage="url('')"
+        div.style.backgroundImage="url('/images/img"+i+".jpg')";
+        container.appendChild(div);
+    }
+    display();
+}
+
+d.onclick=function(){
+    if(p>-nbr+1)
+        p--;
+    container.style.transform = "translate("+ p * 800 + "px)";
+    container.style.transition = "all 0.5s ease";
+    display();
+}
+g.onclick=function(){
+    if(p<0)
+        p++;
+    container.style.transform = "translate("+ p * 800 + "px)";
+    container.style.transition = "all 0.5s ease";
+    display();
+}
+
+
+function display(){
+    if (p == -nbr+1) {
+        d.style.visibility = "hidden";
+    }
+    else {
+        d.style.visibility = "visible";
+    }
+
+    if (p == 0) {
+        g.style.visibility = "hidden";
+    }
+    else {
+        g.style.visibility = "visible";
     }
 }
